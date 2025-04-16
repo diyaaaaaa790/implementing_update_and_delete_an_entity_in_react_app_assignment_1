377dd5d628c8de043d6aed89b50b1ad6cb604f81
DIYA <diya94816@gmail.com>
2025-04-16 08:51:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import UpdateItem from "./components/UpdateItem";
-
-// use the following link to get the data
-// `/doors` will give you all the doors, to get a specific door use `/doors/1`.
-const API_URI = `http://${import.meta.env.VITE_API_URI}/doors`;
+import React from 'react';
+import UpdateItem from './UpdateItem';
 
 function App() {
-  // Get the existing item from the server
-  // const [item, setItem] = useState(null);
-  // pass the item to UpdateItem as a prop
-
-  return <UpdateItem />;
+  return (
+    <div className="App">
+      <UpdateItem itemId="123" /> {/* Replace 123 with actual item ID */}
+    </div>
+  );
 }
 
 export default App;
